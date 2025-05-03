7429e0a1266e33f13693fd507e0bfa7b63a5291a
zloow1009 <zloow1009@gamil.com>
2025-05-03 23:39:15 +0900

    fix: 입력란, 결과란 보드형태로 구성

--- src/components/main/Input.jsx
+++ src/components/main/Input.jsx
@@ -1,11 +1,22 @@
-export default function Input( {setUserInputValue}) {
-    const handleSubmit = (e) => {
-        setUserInputValue(e.target.value);
-    }
-    
-    return (
-        <div className="input">
-            <input type="text" onChange={handleSubmit}/>
-        </div>
-    )
+export default function Input({ setUserInputValue }) {
+  const handleSubmit = (e) => {
+    setUserInputValue(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="input-row">
+        <div className="input-board-block" data-index="00"></div>
+        <div className="input-board-block" data-index="01"></div>
+        <div className="input-board-block" data-index="02"></div>
+        <div className="input-board-block" data-index="03"></div>
+        <div className="input-board-block" data-index="04"></div>
+        <div className="input-board-block" data-index="05"></div>
+      </div>
+
+      {/* <div className="input-row">
+        <input type="text" onChange={handleSubmit} />
+      </div> */}
+    </>
+  );
 }
--- src/components/main/Result.jsx
+++ src/components/main/Result.jsx
@@ -1,7 +1,15 @@
 export default function Result({ userInputValue }) {
-    return (
-        <div className="result">
-            {userInputValue}
-        </div>
-    )
+  return (
+    // <div className="result">
+    //     {userInputValue}
+    // </div>
+    <div className="result-row">
+      <div className="result-board-block" data-index="00"></div>
+      <div className="result-board-block" data-index="01"></div>
+      <div className="result-board-block" data-index="02"></div>
+      <div className="result-board-block" data-index="03"></div>
+      <div className="result-board-block" data-index="04"></div>
+      <div className="result-board-block" data-index="05"></div>
+    </div>
+  );
 }
