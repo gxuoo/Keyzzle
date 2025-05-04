46fde06d4cae034c19cc9f8de3fabeeac528ce4e
zloow1009 <zloow1009@gamil.com>
2025-05-04 19:29:16 +0900

    feat: 위치까지 맞으면 green, 위치는 맞지 않지만 포함된 글자면 yellow로 결과값 표시

--- src/components/main/Result.jsx
+++ src/components/main/Result.jsx
@@ -1,11 +1,34 @@
 export default function Result({ resultValue }) {
-  const resultBlocks = Array.from({ length: 6 }, (_, i) => ({
-    id: `result-block-${i}`,
-  }));
+  const answer = "GREEDY";
+
+  const resultBlocks = Array.from({ length: 6 }, (_, i) => {
+    let bgColor, textColor;
+    if (answer[i] === resultValue[i]) {
+      bgColor = "#6AAA64";
+      textColor = "white";
+    } else if (answer.includes(resultValue[i])) {
+      bgColor = "#C9B458";
+      textColor = "white";
+    } else {
+      bgColor = "white";
+      textColor = "black";
+    }
+    return {
+      id: `result-block-${i}`,
+      bgColor,
+      textColor,
+    };
+  });
+
   return (
     <div className="result-row">
       {resultBlocks.map((block, i) => (
-        <div key={block.id} className="result-board-block" data-index={`0${i}`}>
+        <div
+          key={block.id}
+          className="result-board-block"
+          data-index={`0${i}`}
+          style={{ backgroundColor: block.bgColor, color: block.textColor }}
+        >
           {resultValue[i]}
         </div>
       ))}
