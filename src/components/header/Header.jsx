ec50bc9ab96872d04774c245cf28068aed39acbc
gxuoo <rbdud189@naver.com>
2025-05-09 00:24:08 +0900

    refactor: App, Header 컴포넌트 및 css 수정
    1. 배경색을 그리디 색상으로 변경
    2. Header 컴포넌트에 동아리명, 제작자 추가, 위치 설정

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,9 +2,15 @@ import "../../styles/header.css";
 
 export default function Header() {
     return (
-        <header className="header">
+        <header className="header-container">
+            <div className="header-greedy">
+                We are Greedy
+            </div>
             <div className="header-title">
-                <h1>Keyzzle</h1>
+                <h1>KEYZZLE</h1>
+            </div>
+            <div className="header-developer">
+                제작자: 신지우, 임규영, 정창우
             </div>
         </header>
     )
