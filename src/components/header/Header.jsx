54f366317085ae991b84ca6834285d989685655c
zldn109 <zloow1009@gamil.com>
2025-05-10 02:01:33 +0900

    refactor: 폰트 추가 및 제목 수정

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,13 @@
 import "../../styles/header.css";
 
 export default function Header() {
-    return (
-        <header className="header-container">
-            <div className="header-greedy">
-                We are Greedy
-            </div>
-            <div className="header-title">
-                <h1>KEYZZLE</h1>
-            </div>
-            <div className="header-developer">
-                제작자: 신지우, 임규영, 정창우
-            </div>
-        </header>
-    )
+  return (
+    <header className="header-container">
+      <div className="header-greedy">We are Greedy</div>
+      <div className="header-title">
+        <h1>KeyZzle</h1>
+      </div>
+      <div className="header-developer">제작자: 신지우, 임규영, 정창우</div>
+    </header>
+  );
 }
