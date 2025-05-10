5804e42512acc0bcea6101b2b71cc11bb870df23
gxuoo <rbdud189@naver.com>
2025-05-11 01:32:43 +0900

    feat: Answer 컴포넌트 기능 구현
    1. 사용자가 입력한 6자리 알파벳과 정답인 GREEDY를 각 자리 마다 비교하여 정답 유무를 체크합니다.
    
    2. 숫자 야구 게임 방식을 적용하여 자리와 값이 일치하면 초록색으로 효과를 주고, 값만 일치하면 노란색으로 효과를 줍니다.
    
    3. 정답을 맞출 시에는 GameResultModal로 컴포넌트를 변경합니다.

--- src/components/main/playing/Answer.jsx
+++ src/components/main/playing/Answer.jsx
@@ -1,9 +1,36 @@
+import { useEffect } from "react";
 import "../../../styles/main/playing/Answer.css";
 
-export default function Answer() {
+const answer = "GREEDY";
+
+export default function Answer({ userInputValue, setGameState }) {
+    useEffect(() => {
+        if (userInputValue.length === 6) {
+            const userAnswer = userInputValue.join("");
+            if (userAnswer === answer) {
+                setGameState("result");
+            }
+        }
+    }, [userInputValue, setGameState]);
+
     return (
-        <div className="answer">
-            G R E E D Y
+        <div className="answer-container">
+            {answer.split("").map((letter, index) => {
+                // 정확한 위치와 값이 맞는 경우 => (strike)
+                const isStrike = userInputValue[index] === letter;
+
+                // 해당 값이 정답에 포함되어 있고, 정확한 위치가 아닌 경우 => (ball)
+                const isBall = userInputValue.includes(letter) && answer.includes(letter);
+
+                return (
+                    <div
+                        key={index}
+                        className={`answer-letter ${isStrike ? 'strike' : isBall ? 'ball' : ''}`}
+                    >
+                        {letter}
+                    </div>
+                );
+            })}
         </div>
     );
 }
