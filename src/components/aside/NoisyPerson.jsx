9f2e61229d7b4a1392341f606ef1a52ef6fbfc39
gxuoo <rbdud189@naver.com>
2025-05-11 05:07:18 +0900

    Feat: 오른쪽 aside 컴포넌트 구현
    떠든 사람은 그리디 메인테이너, 학식 메뉴는 학생회관 식당 메뉴와 진관키친 메뉴를 넣어봤습니다.

--- src/components/aside/NoisyPerson.jsx
+++ src/components/aside/NoisyPerson.jsx
@@ -4,6 +4,11 @@ export default function NoisyPerson() {
     return (
         <div className="noisy-person">
             <h1>떠든 사람</h1>
+            <p>이승용</p>
+            <p>김범수</p>
+            <p>김수민</p>
+            <p>원태연</p>
+            <p>김주환</p>
         </div>
     );
 }
