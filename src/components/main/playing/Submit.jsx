c40f0801a60c287d3c72d09c2a5c4fef53131511
gxuoo <rbdud189@naver.com>
2025-05-12 20:56:54 +0900

    fix: 정답 제출 버튼 삭제
    정답 제출 버튼이 불필요한 게임 과정이라고 생각되어 삭제했습니다.

--- src/components/main/playing/Submit.jsx
+++ src/components/main/playing/Submit.jsx
@@ -2,27 +2,24 @@ import "../../../styles/main/playing/Submit.css";
 import { useState, useEffect } from "react";
 
 export default function Submit({ keyMap, userInputValue, setUserInputValue }) {
-  const [isSubmitButtonClicked, setIsSubmitButtonClicked] = useState(false);
   const inputLines = Array.from({ length: 6 }, (_, i) => ({
     id: `input-block-${i}`,
   }));
 
   useEffect(() => {
-    if (isSubmitButtonClicked) {
-      const handleKeydown = (e) => {
-        const key = e.key.toUpperCase();
-        if (!/^[a-zA-Z]$/.test(key)) return;
-        if (userInputValue.length >= 6) return;
+    const handleKeydown = (e) => {
+      const key = e.key.toUpperCase();
+      if (!/^[a-zA-Z]$/.test(key)) return;
+      if (userInputValue.length >= 6) return;
 
-        // keyMap을 사용하여 키를 매핑하고 업데이트
-        const mappedKey = keyMap ? keyMap[key] : key;
-        setUserInputValue((prevInputValue) => [...prevInputValue, mappedKey]);
-      };
+      // keyMap을 사용하여 키를 매핑하고 업데이트
+      const mappedKey = keyMap ? keyMap[key] : key;
+      setUserInputValue((prevInputValue) => [...prevInputValue, mappedKey]);
+    };
 
-      window.addEventListener("keydown", handleKeydown);
-      return () => window.removeEventListener("keydown", handleKeydown);
-    }
-  }, [isSubmitButtonClicked, userInputValue, keyMap]);
+    window.addEventListener("keydown", handleKeydown);
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, [userInputValue, keyMap]);
 
   useEffect(() => {
     if (userInputValue.length === 6) {
@@ -34,25 +31,17 @@ export default function Submit({ keyMap, userInputValue, setUserInputValue }) {
 
   return (
     <div>
-      {!isSubmitButtonClicked ? (
-        <div className="submit-button">
-          <button type="button" onClick={() => setIsSubmitButtonClicked(true)}>
-            정답 제출!
-          </button>
-        </div>
-      ) : (
-        <div className="input-row">
-          {inputLines.map((line, i) => (
-            <div
-              key={line.id}
-              className="input-board-line"
-              data-index={`0${i}`}
-            >
-              {userInputValue[i]}
-            </div>
-          ))}
-        </div>
-      )}
+      <div className="input-row">
+        {inputLines.map((line, i) => (
+          <div
+            key={line.id}
+            className="input-board-line"
+            data-index={`0${i}`}
+          >
+            {userInputValue[i]}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
